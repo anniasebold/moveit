b87af748926f4e0a3e7c5cd14a80aade2252af38
Annia Sebold <anniasebold3.0@gmail.com>
2021-02-27 09:32:11 -0400

    Aula 3

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,60 @@
+import { useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+import { createContext } from 'react';
+
+interface Challenge {
+  // pode mudar depois p/ string
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children }) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  function levelUp() {
+    setLevel(level + 1)
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge)
+  }
+
+  return (
+    <ChallengesContext.Provider value={{
+      level,
+      currentExperience,
+      challengesCompleted,
+      levelUp,
+      startNewChallenge,
+      activeChallenge
+    }}
+    >
+      {children}
+    </ChallengesContext.Provider >
+  );
+}
+
